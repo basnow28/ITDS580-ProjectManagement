ff0204dcddccebcfaf86d345153d768a617e0fe1
Barbara Rekowska <barbara.rekowska@toq.io>
2025-04-06 17:13:34 +0200

    :tada: CRM-18 Add new endpoint to set the habit as completed

--- routes/habitRoutes.js
+++ routes/habitRoutes.js
@@ -5,5 +5,6 @@ const habitController = require("../controller/habitController");
 router.get("/ok", habitController.ok);
 router.post("", habitController.createHabit);
 router.get("", habitController.getUserHabits)
+router.put("/:habitId/day/:dayNumber/completed", habitController.updateHabitDayCompletion)
 
 module.exports = router
